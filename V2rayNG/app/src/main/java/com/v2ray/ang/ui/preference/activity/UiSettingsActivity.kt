318704ceae0f5c59e00cfc2bd30b62cae4fefcdb
HatsuneMikuUwU <146099520+HatsuneMikuUwU@users.noreply.github.com>
2026-05-26 17:55:09 +0700

    fix

--- V2rayNG/app/src/main/java/com/v2ray/ang/ui/preference/activity/UiSettingsActivity.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/ui/preference/activity/UiSettingsActivity.kt
@@ -17,7 +17,7 @@ import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.v2ray.ang.AppConfig
 import com.v2ray.ang.R
-import com.v2ray.ang.extension.toastSuccess
+import com.v2ray.ang.extension.alertSuccess
 import com.v2ray.ang.handler.MmkvManager
 import com.v2ray.ang.helper.MmkvPreferenceDataStore
 import com.v2ray.ang.ui.BaseActivity
@@ -80,9 +80,7 @@ class UiSettingsActivity : BaseActivity() {
                         val savedUri = saveToCache(cacheUri, "home_banner_")
                         MmkvManager.encodeSettings(AppConfig.PREF_CUSTOM_HOME_BANNER_URI, savedUri.toString())
                         broadcastHomeBannerChanged()
-                        requireContext().toastSuccess(getString(R.string.home_banner_updated))
-                    } catch (e: Exception) {
-                        requireContext().toastSuccess(getString(R.string.home_banner_updated))
+                        requireContext().alertSuccess(getString(R.string.home_banner_updated), title = getString(R.string.title_alerter_success))
                     }
                 } else if (result.resultCode == UCrop.RESULT_ERROR) {
                     val err = UCrop.getError(result.data!!)
@@ -100,9 +98,7 @@ class UiSettingsActivity : BaseActivity() {
                         val savedUri = saveToCache(cacheUri, "profile_banner_")
                         MmkvManager.encodeSettings(AppConfig.PREF_PROFILE_BANNER_URI, savedUri.toString())
                         broadcastProfileChanged()
-                        requireContext().toastSuccess(getString(R.string.custom_banner_profile_set))
-                    } catch (e: Exception) {
-                        requireContext().toastSuccess(getString(R.string.custom_banner_profile_set))
+                        requireContext().alertSuccess(getString(R.string.custom_banner_profile_set), title = getString(R.string.title_alerter_success))
                     }
                 } else if (result.resultCode == UCrop.RESULT_ERROR) {
                     val err = UCrop.getError(result.data!!)
@@ -225,12 +221,10 @@ class UiSettingsActivity : BaseActivity() {
                             deleteOldFile(savedUri)
                             MmkvManager.encodeSettings(AppConfig.PREF_PROFILE_BANNER_URI, "")
                             broadcastProfileChanged()
-                            requireContext().toastSuccess(getString(R.string.custom_banner_profile_set))
+                            requireContext().alertSuccess(getString(R.string.delete_custom_banner_profile_summary), title = getString(R.string.title_alerter_success))
                         }
                         .setNegativeButton(android.R.string.cancel, null)
                         .showBlur()
-                } else {
-                    requireContext().toastSuccess(getString(R.string.delete_custom_banner_profile_summary))
                 }
                 true
             }
@@ -263,12 +257,10 @@ class UiSettingsActivity : BaseActivity() {
                             deleteOldFile(savedUri)
                             MmkvManager.encodeSettings(AppConfig.PREF_CUSTOM_HOME_BANNER_URI, "")
                             broadcastHomeBannerChanged()
-                            requireContext().toastSuccess(getString(R.string.home_banner_updated))
+                            requireContext().alertSuccess(getString(R.string.home_banner_delete_summary), title = getString(R.string.title_alerter_success))
                         }
                         .setNegativeButton(android.R.string.cancel, null)
                         .showBlur()
-                } else {
-                    requireContext().toastSuccess(getString(R.string.home_banner_delete_summary))
                 }
                 true
             }
