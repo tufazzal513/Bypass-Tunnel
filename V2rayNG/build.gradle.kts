fb1a84faa0a0d3c0f8023edfb9c21717b88e9672
tufazzal513 <mdtufazzal513@gmail.com>
2026-07-14 00:41:59 +0600

    Add Google Services plugin version 4.4.1

--- V2rayNG/build.gradle.kts
+++ V2rayNG/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
+    id("com.google.gms.google-services") version "4.4.1" apply false
 }
 
 buildscript {
